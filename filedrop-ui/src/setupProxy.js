a85dcfcb4b3c4e34b8cd2a1c6dcb4e5a3986c3ad
pvavhad <pvavhad@Pankajs-MacBook-Pro.local>
2018-11-24 12:07:46 -0800

    initial commit

---
+++ filedrop-ui/src/setupProxy.js
@@ -0,0 +1,13 @@
+// const proxy = require('http-proxy-middleware');
+// module.exports = function(app) {
+// app.use(proxy('*', 
+//                { target: 'http://localhost:5000/' }
+//   ));
+// }
+
+
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(proxy('/api/*', { target: 'http://localhost:5000/' }));
+};
